5963faef89fe48cb4e7787bea69aad86ba6bbc40
stephanewong <stephane.wong1@gmail.com>
2021-04-22 22:06:52 +0200

    can list bucket and files

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -9,6 +9,18 @@ class AWSUtil:
     def get_resource_s3(self):
         return self._s3
 
+    def get_all_buckets(self) -> list[str]:
+        buckets = []
+        for bucket in self._s3.buckets.all():
+            buckets.append(bucket.name)
+        return buckets
+
+    def get_objects_from_bucket(self, bucket_name: str) -> list[str]:
+        files = []
+        for file in self._s3.Bucket(bucket_name).objects.all():
+            files.append(file.key)
+        return files
+
     def __init__(self):
         self._session = boto3.Session()
         self._s3 = boto3.resource('s3')
